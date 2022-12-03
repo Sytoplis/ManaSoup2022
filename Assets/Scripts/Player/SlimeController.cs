307f3e4ddbb7955779e37fa81e6e751f5c3d1ac1
Kuma <kuma@kuma.com>
2022-12-03 16:58:16 +0100

    Slime Controller added

---
+++ Assets/Scripts/Player/SlimeController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class SlimeController : MovementController
+{
+
+    [Space]
+    [SerializeField][Range(-1, 1)] private float maxLeftMovement;
+    [SerializeField][Range(-1, 1)] private float maxRightMovement;
+    [SerializeField][Range(0, 100)] private float jumpProbability;
+
+    internal override float DecideGravity()
+    {
+
+        if (velocity.y <= 0)
+        {
+            return fallGravity;
+        }
+        // else if (velocity.y > 5)
+        // {
+        //     return lowJumpGravity;
+        // }
+        return upGravity;
+
+    }
+
+    internal override float GetHorizontalInput()
+    {
+        return Random.Range(maxLeftMovement, maxRightMovement);
+    }
+
+    internal override bool GetJumpInput()
+    {
+        var random = Random.Range(0,100000f);
+        if(jumpProbability < random){
+            return true;
+        }
+        return false;
+    }
+
+    internal override bool GetDoubleJumpInput()
+    {
+        return false;
+    }
+    internal override void JumpEffects()
+    {
+        
+    }
+}
